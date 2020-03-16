4ff1ebd5a5cb36f4f5fb6be38dd2a47a7e035f89
Andrew Yang <qmz0217@outlook.com>
2020-03-16 18:38:29 -0400

    Add project files.

---
+++ Mad Minute Monday/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Mad_Minute_Monday
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.lblnumerator = new MaterialSkin.Controls.MaterialSingleLineTextField();
+			this.lbldenominator = new MaterialSkin.Controls.MaterialSingleLineTextField();
+			this.lblSign = new MaterialSkin.Controls.MaterialLabel();
+			this.lbldenom1 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+			this.lblnume1 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+			this.ansdenom = new MaterialSkin.Controls.MaterialSingleLineTextField();
+			this.ansnume = new MaterialSkin.Controls.MaterialSingleLineTextField();
+			this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+			this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+			this.SuspendLayout();
+			// 
+			// lblnumerator
+			// 
+			this.lblnumerator.Depth = 0;
+			this.lblnumerator.Enabled = false;
+			this.lblnumerator.Hint = "";
+			this.lblnumerator.Location = new System.Drawing.Point(74, 171);
+			this.lblnumerator.Margin = new System.Windows.Forms.Padding(6);
+			this.lblnumerator.MouseState = MaterialSkin.MouseState.HOVER;
+			this.lblnumerator.Name = "lblnumerator";
+			this.lblnumerator.PasswordChar = '\0';
+			this.lblnumerator.SelectedText = "";
+			this.lblnumerator.SelectionLength = 0;
+			this.lblnumerator.SelectionStart = 0;
+			this.lblnumerator.Size = new System.Drawing.Size(50, 41);
+			this.lblnumerator.TabIndex = 0;
+			this.lblnumerator.Text = "  1";
+			this.lblnumerator.UseSystemPasswordChar = false;
+			// 
+			// lbldenominator
+			// 
+			this.lbldenominator.Depth = 0;
+			this.lbldenominator.Enabled = false;
+			this.lbldenominator.Hint = "";
+			this.lbldenominator.ImeMode = System.Windows.Forms.ImeMode.Off;
+			this.lbldenominator.Location = new System.Drawing.Point(74, 256);
+			this.lbldenominator.Margin = new System.Windows.Forms.Padding(6);
+			this.lbldenominator.MouseState = MaterialSkin.MouseState.HOVER;
+			this.lbldenominator.Name = "lbldenominator";
+			this.lbldenominator.PasswordChar = '\0';
+			this.lbldenominator.SelectedText = "";
+			this.lbldenominator.SelectionLength = 0;
+			this.lbldenominator.SelectionStart = 0;
+			this.lbldenominator.Size = new System.Drawing.Size(50, 41);
+			this.lbldenominator.TabIndex = 1;
+			this.lbldenominator.Text = "  1";
+			this.lbldenominator.UseSystemPasswordChar = false;
+			// 
+			// lblSign
+			// 
+			this.lblSign.AutoSize = true;
+			this.lblSign.Depth = 0;
+			this.lblSign.Font = new System.Drawing.Font("Roboto", 11F);
+			this.lblSign.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.lblSign.Location = new System.Drawing.Point(110, 217);
+			this.lblSign.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+			this.lblSign.MouseState = MaterialSkin.MouseState.HOVER;
+			this.lblSign.Name = "lblSign";
+			this.lblSign.Size = new System.Drawing.Size(34, 37);
+			this.lblSign.TabIndex = 2;
+			this.lblSign.Text = "+";
+			// 
+			// lbldenom1
+			// 
+			this.lbldenom1.Depth = 0;
+			this.lbldenom1.Enabled = false;
+			this.lbldenom1.Hint = "";
+			this.lbldenom1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+			this.lbldenom1.Location = new System.Drawing.Point(162, 256);
+			this.lbldenom1.Margin = new System.Windows.Forms.Padding(6);
+			this.lbldenom1.MouseState = MaterialSkin.MouseState.HOVER;
+			this.lbldenom1.Name = "lbldenom1";
+			this.lbldenom1.PasswordChar = '\0';
+			this.lbldenom1.SelectedText = "";
+			this.lbldenom1.SelectionLength = 0;
+			this.lbldenom1.SelectionStart = 0;
+			this.lbldenom1.Size = new System.Drawing.Size(50, 41);
+			this.lbldenom1.TabIndex = 4;
+			this.lbldenom1.Text = "  1";
+			this.lbldenom1.UseSystemPasswordChar = false;
+			// 
+			// lblnume1
+			// 
+			this.lblnume1.Depth = 0;
+			this.lblnume1.Enabled = false;
+			this.lblnume1.Hint = "";
+			this.lblnume1.Location = new System.Drawing.Point(162, 171);
+			this.lblnume1.Margin = new System.Windows.Forms.Padding(6);
+			this.lblnume1.MouseState = MaterialSkin.MouseState.HOVER;
+			this.lblnume1.Name = "lblnume1";
+			this.lblnume1.PasswordChar = '\0';
+			this.lblnume1.SelectedText = "";
+			this.lblnume1.SelectionLength = 0;
+			this.lblnume1.SelectionStart = 0;
+			this.lblnume1.Size = new System.Drawing.Size(50, 41);
+			this.lblnume1.TabIndex = 3;
+			this.lblnume1.Text = "  1";
+			this.lblnume1.UseSystemPasswordChar = false;
+			// 
+			// ansdenom
+			// 
+			this.ansdenom.Depth = 0;
+			this.ansdenom.Hint = "";
+			this.ansdenom.ImeMode = System.Windows.Forms.ImeMode.Off;
+			this.ansdenom.Location = new System.Drawing.Point(322, 256);
+			this.ansdenom.Margin = new System.Windows.Forms.Padding(6);
+			this.ansdenom.MouseState = MaterialSkin.MouseState.HOVER;
+			this.ansdenom.Name = "ansdenom";
+			this.ansdenom.PasswordChar = '\0';
+			this.ansdenom.SelectedText = "";
+			this.ansdenom.SelectionLength = 0;
+			this.ansdenom.SelectionStart = 0;
+			this.ansdenom.Size = new System.Drawing.Size(70, 41);
+			this.ansdenom.TabIndex = 6;
+			this.ansdenom.UseSystemPasswordChar = false;
+			this.ansdenom.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ansdenom_KeyDown);
+			// 
+			// ansnume
+			// 
+			this.ansnume.Depth = 0;
+			this.ansnume.Hint = "";
+			this.ansnume.Location = new System.Drawing.Point(322, 171);
+			this.ansnume.Margin = new System.Windows.Forms.Padding(6);
+			this.ansnume.MouseState = MaterialSkin.MouseState.HOVER;
+			this.ansnume.Name = "ansnume";
+			this.ansnume.PasswordChar = '\0';
+			this.ansnume.SelectedText = "";
+			this.ansnume.SelectionLength = 0;
+			this.ansnume.SelectionStart = 0;
+			this.ansnume.Size = new System.Drawing.Size(70, 41);
+			this.ansnume.TabIndex = 5;
+			this.ansnume.UseSystemPasswordChar = false;
+			// 
+			// materialLabel1
+			// 
+			this.materialLabel1.AutoSize = true;
+			this.materialLabel1.Depth = 0;
+			this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+			this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.materialLabel1.Location = new System.Drawing.Point(248, 217);
+			this.materialLabel1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+			this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+			this.materialLabel1.Name = "materialLabel1";
+			this.materialLabel1.Size = new System.Drawing.Size(33, 37);
+			this.materialLabel1.TabIndex = 7;
+			this.materialLabel1.Text = "=";
+			// 
+			// materialFlatButton1
+			// 
+			this.materialFlatButton1.AutoSize = true;
+			this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.materialFlatButton1.Depth = 0;
+			this.materialFlatButton1.Location = new System.Drawing.Point(424, 217);
+			this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+			this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+			this.materialFlatButton1.Name = "materialFlatButton1";
+			this.materialFlatButton1.Primary = false;
+			this.materialFlatButton1.Size = new System.Drawing.Size(47, 36);
+			this.materialFlatButton1.TabIndex = 9;
+			this.materialFlatButton1.Text = "✓";
+			this.materialFlatButton1.UseVisualStyleBackColor = true;
+			this.materialFlatButton1.Click += new System.EventHandler(this.materialFlatButton1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(714, 363);
+			this.Controls.Add(this.materialFlatButton1);
+			this.Controls.Add(this.materialLabel1);
+			this.Controls.Add(this.ansdenom);
+			this.Controls.Add(this.ansnume);
+			this.Controls.Add(this.lbldenom1);
+			this.Controls.Add(this.lblnume1);
+			this.Controls.Add(this.lblSign);
+			this.Controls.Add(this.lbldenominator);
+			this.Controls.Add(this.lblnumerator);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(6);
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Sizable = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Mad Minute Monday";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private MaterialSkin.Controls.MaterialSingleLineTextField lblnumerator;
+		private MaterialSkin.Controls.MaterialSingleLineTextField lbldenominator;
+		private MaterialSkin.Controls.MaterialLabel lblSign;
+		private MaterialSkin.Controls.MaterialSingleLineTextField lbldenom1;
+		private MaterialSkin.Controls.MaterialSingleLineTextField lblnume1;
+		private MaterialSkin.Controls.MaterialSingleLineTextField ansdenom;
+		private MaterialSkin.Controls.MaterialSingleLineTextField ansnume;
+		private MaterialSkin.Controls.MaterialLabel materialLabel1;
+		private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+	}
+}
+
---
+++ Mad Minute Monday/Form1.cs
@@ -0,0 +1,145 @@
+﻿using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using System.Globalization;
+
+//Update Mar 16th, 2020
+
+namespace Mad_Minute_Monday
+{
+	public partial class Form1 : MaterialForm
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			var materialSkinManager = MaterialSkinManager.Instance;
+			materialSkinManager.AddFormToManage(this);
+			materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+			materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue200, Primary.Blue400, Primary.Cyan50, Accent.LightBlue200, TextShade.WHITE);
+		}
+		static Random rd = new Random();
+		static int nume = rd.Next(1, 10);
+		static int denom = rd.Next(nume, 10);
+		static int nume1 = rd.Next(1, 10);
+		static int denom1 = rd.Next(nume1, 10);
+		void generateNew()
+		{
+			while (nume==denom)
+				denom = rd.Next(1, 10);
+			lblnumerator.Text = "" + nume.ToString();
+			lbldenominator.Text = "" + denom.ToString();
+
+
+			
+			while (nume1 == denom1)
+				denom1 = rd.Next(1, 10);
+			lblnume1.Text = "" + nume1.ToString();
+			lbldenom1.Text = "" + denom1.ToString();
+		}
+
+		static char[] signList = { '+', '-', 'x', '÷' };
+		//int index = rd.Next(signList.Length);
+		int Sindex = 0;
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			generateNew();
+
+			lblSign.Text = signList[0].ToString();
+		}
+		static int gcd(int a, int b)
+		{
+			//find the gcd using the Euclid’s algorithm
+			while (a != b)
+				if (a < b) b = b - a;
+				else a = a - b;
+			//since at this point a=b, the gcd can be either of them
+			//it is necessary to pass the gcd to the main function
+			return (a);
+		}
+		static int division(int a, int b)
+		{
+			int remainder = a, quotient = 0;
+			while (remainder >= b)
+			{
+				remainder = remainder - b;
+				quotient++;
+			}
+			return (quotient);
+		}
+		int aDenom { get; set; }
+		int aNume { get; set; }
+		void checkAnswer()
+		{
+			/*switch (lblSign.Text)
+			{
+				case "+":
+					double answer = (nume / denom) + (nume1 / denom1);
+					if (doubleans.Text == Math.Round(answer, 1).ToString())
+					{
+						MessageBox.Show("Correct!");
+					}
+					if (string.IsNullOrEmpty(doubleans.Text))
+					{
+						MessageBox.Show("Empty");
+					}
+					break;
+			}*/
+			//double firstFraction = Convert.ToDouble(Convert.ToDouble(lblnumerator.Text) / Convert.ToDouble(lbldenominator.Text));
+			//double lastFraction = Convert.ToDouble(Convert.ToDouble(lblnume1.Text) / Convert.ToDouble(lbldenom1.Text));
+			//double answerB = Convert.ToDouble(firstFraction+lastFraction);
+			//string answer = Dec2Frac(Math.Round(answerB,1));
+			aDenom = Convert.ToInt32(lbldenominator.Text) * Convert.ToInt32(lbldenom1.Text);
+			aNume = (Convert.ToInt32(lblnumerator.Text) * Convert.ToInt32(lbldenom1.Text)) + (Convert.ToInt32(lbldenominator.Text) * Convert.ToInt32(lblnume1.Text));
+
+			int divisor = gcd(aNume, aDenom);
+			if (divisor != 1)
+			{
+				aNume = aNume / divisor;
+				aDenom = aDenom / divisor;
+			}
+			string answer = aNume + "/" + aDenom;
+
+			if (ansnume.Text==aNume.ToString()&&ansdenom.Text==aDenom.ToString())
+			{
+				MessageBox.Show("Correct!");
+				generateNew();
+			}
+			else
+			{
+				MessageBox.Show(answer.ToString()) ;
+				ansnume.Clear(); ansdenom.Clear();
+				generateNew();
+			}
+		}
+
+		private void ansdenom_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				checkAnswer();
+			}
+		}
+
+		private void doubleans_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (e.KeyChar == (char)Keys.Enter)
+			{
+				checkAnswer();
+			}
+		}
+
+		private void materialFlatButton1_Click(object sender, EventArgs e)
+		{
+			checkAnswer();
+		}
+	}
+}
